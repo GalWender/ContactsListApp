41703cf78b52c2a8f4f877c03209fe3209dc21c3
Gal Wender <galwender@gmail.com>
2024-02-22 00:27:41 +0200

    navigation is setup as well as some bug fixes to expo contacts

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import AppNavigator from './src/navigation/app-navigator';
+import { NavigationContainer } from '@react-navigation/native';
+
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AppNavigator />
+    </NavigationContainer>
   );
 }
 
